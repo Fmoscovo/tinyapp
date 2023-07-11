81c819215096ab7ac4e7f890a98022562ff92914
Filipe <moscovo.filipe@gmail.com>
2023-07-10 23:03:03 -0600

    Implement generateRandomString function and update express_server.js and urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -3,35 +3,70 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+//   middleware
+app.use(express.urlencoded({ extended: true }));
 
+// function to generate a random string of 6 characters
+function generateRandomString() {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const length = 6;
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+}
+
+////////////routes////////////
 const urlDatabase = {
+  // database of URLs
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+// GET request for the homepage
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Hello! Welcome to the TinyApp!");
 });
 
+// GET request to display the list of URLs
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// GET request to retrieve URL database in JSON format
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// GET request to display a simple "Hello World" page in HTML
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// GET request to render the URL submission form
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// GET request to display the details of a specific URL
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
   };
   res.render("urls_show", templateVars);
+});
+
+// POST request adding a new URL to the database
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
